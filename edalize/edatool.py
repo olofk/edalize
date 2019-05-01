a9c2e0887607c28c1ef2798a3d13bc7ef0a5df18
Philipp Wagner <mail@philipp-wagner.com>
2019-05-01 14:28:31 +0100

    Factor out *.f file writing
    File lists are commonly used in EDA tools, factor its generation out
    into a common subroutine will help other backends to re-use this code.

--- edalize/edatool.py
+++ edalize/edatool.py
@@ -294,3 +294,32 @@ class Edatool(object):
         except subprocess.CalledProcessError:
             _s = "'{}' exited with an error code"
             raise RuntimeError(_s.format(cmd))
+
+    def _write_fileset_to_f_file(self, output_file):
+        """ Write a file list (*.f) file
+
+        Returns a list of all files which were not added to the *.f file
+        """
+
+        with open(output_file, 'w') as f:
+            unused_files = []
+            (src_files, incdirs) = self._get_fileset_files()
+
+            for key, value in self.vlogdefine.items():
+                define_str = self._param_value_str(param_value = value)
+                f.write('+define+{}={}\n'.format(key, define_str))
+
+            for key, value in self.vlogparam.items():
+                param_str = self._param_value_str(param_value = value, str_quote_style = '"')
+                f.write('+parameter+{}.{}={}\n'.format(self.toplevel, key, param_str))
+
+            for id in incdirs:
+                f.write("+incdir+" + id + '\n')
+
+            for src_file in src_files:
+                if (src_file.file_type.startswith("verilogSource") or src_file.file_type.startswith("systemVerilogSource")):
+                    f.write(src_file.name + '\n')
+                else:
+                    unused_files.append(src_file)
+
+            return unused_files
--- edalize/vcs.py
+++ edalize/vcs.py
@@ -14,26 +14,7 @@ class Vcs(Edatool):
     argtypes = ['plusarg', 'vlogdefine', 'vlogparam']
 
     def configure_main(self):
-        f = open(os.path.join(self.work_root, self.name+'.scr'),'w')
-
-        (src_files, incdirs) = self._get_fileset_files()
-        for key, value in self.vlogdefine.items():
-            f.write('+define+{}={}\n'.format(key, self._param_value_str(value, '')))
-
-        for key, value in self.vlogparam.items():
-            f.write('+parameter+{}.{}={}\n'.format(self.toplevel, key, self._param_value_str(value, '"')))
-        for id in incdirs:
-            f.write("+incdir+" + id+'\n')
-        for src_file in src_files:
-            if (src_file.file_type.startswith("verilogSource") or src_file.file_type.startswith("systemVerilogSource")):
-                f.write(src_file.name+'\n')
-            elif src_file.file_type == 'user':
-                pass
-            else:
-                _s = "{} has unknown file type '{}'"
-                logger.warning(_s.format(src_file.name, src_file.file_type))
-
-        f.close()
+        self._write_fileset_to_f_file(os.path.join(self.work_root, self.name + '.scr'))
 
         plusargs = []
         if self.plusarg:
