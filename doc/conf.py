b981ec39bdba7fee5c0596db075173d1a48cb57d
Colin Marquardt <cmarqu42@gmail.com>
2019-11-17 13:28:35 +0100

    Add sphinx.ext.napoleon to allow for nicer docs.

--- doc/conf.py
+++ doc/conf.py
@@ -42,6 +42,7 @@ release = '0.1.3'
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.viewcode',
+    'sphinx.ext.napoleon',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
