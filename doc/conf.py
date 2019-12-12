f761cacb9f7797f25ab66301ab7d9f589900f2fa
Colin Marquardt <cmarqu42@gmail.com>
2019-12-12 23:21:38 +0100

    Fix sys.path in Sphinx config.

--- doc/conf.py
+++ doc/conf.py
@@ -15,8 +15,8 @@
 from datetime import datetime
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../../edalize'))
-sys.path.insert(0, os.path.abspath('../../edalize/tests/test_vunit/vunit_mock/'))
+sys.path.insert(0, os.path.abspath('..'))
+sys.path.insert(0, os.path.abspath('../tests/test_vunit/vunit_mock/'))
 
 
 # -- Project information -----------------------------------------------------
