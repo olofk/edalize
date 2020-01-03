a568153cacd822954f5b0721579d2dee99348797
DHWinterWolf <17268847+DHWinterWolf@users.noreply.github.com>
2020-01-03 09:52:50 +0000

    Add tests for Quartus with DSE as the P&R tool

--- tests/test_quartus.py
+++ tests/test_quartus.py
@@ -17,8 +17,10 @@ qsys_file = """<?xml version="1.0" encoding="UTF-8"?>
 qsys_fill = {"Standard": "",
              "Pro"     : "tool=\"QsysPro\""}
 
-test_sets = {"Standard": ['ip-generate.cmd', 'quartus_asm.cmd', 'quartus_fit.cmd', 'quartus_map.cmd', 'quartus_sh.cmd', 'quartus_sta.cmd'],
-             "Pro"     : ['qsys-generate.cmd', 'quartus_asm.cmd', 'quartus_fit.cmd', 'quartus_syn.cmd', 'quartus_sh.cmd', 'quartus_sta.cmd']}
+test_sets = {"Standard": {"Quartus": ['ip-generate.cmd', 'quartus_asm.cmd', 'quartus_fit.cmd', 'quartus_map.cmd', 'quartus_sh.cmd', 'quartus_sta.cmd'],
+                          "DSE"    : ['ip-generate.cmd', 'quartus_map.cmd', 'quartus_sh.cmd', 'quartus_dse.cmd']},
+             "Pro"     : {"Quartus": ['qsys-generate.cmd', 'quartus_asm.cmd', 'quartus_fit.cmd', 'quartus_syn.cmd', 'quartus_sh.cmd', 'quartus_sta.cmd'],
+                          "DSE"    : ['qsys-generate.cmd', 'quartus_syn.cmd', 'quartus_sh.cmd', 'quartus_dse.cmd']}}
 
 def test_quartus():
     import os
@@ -36,22 +38,28 @@ def test_quartus():
 
     # Test each edition of Quartus
     for edition in ["Standard", "Pro"]:
-        # Each edition has its own set of representative files    
-        ref_dir      = os.path.join(tests_dir, __name__, edition)
- 
-        # Ensure we test the edition we intend to, even if quartus_sh is present
-        os.environ["FUSESOC_QUARTUS_EDITION"] = edition
-        (backend, args, work_root) = setup_backend(paramtypes, name, tool, tool_options)    
+        for pnr in ["Quartus", "DSE"]:
+            # Each edition and P&R tool has its own set of representative files
+            ref_dir       = os.path.join(tests_dir, __name__, edition)
 
-        # Each edition performs checks on the QSYS files present, so provide
-        # a minimal example
-        with open(os.path.join(work_root, "qsys_file"), 'w') as f:
-            f.write(qsys_file.format(qsys_fill[edition]))
+            if pnr == "DSE":
+                _tool_options = {**tool_options, "pnr": "dse"}
+            else:
+                _tool_options = {**tool_options}
 
-        backend.configure(args)
+            # Ensure we test the edition we intend to, even if quartus_sh is present
+            os.environ["FUSESOC_QUARTUS_EDITION"] = edition
+            (backend, args, work_root) = setup_backend(paramtypes, name, tool, _tool_options)
 
-        compare_files(ref_dir, work_root, ['Makefile',
-                                           name+'.tcl'])
+            # Each edition performs checks on the QSYS files present, so provide
+            # a minimal example
+            with open(os.path.join(work_root, "qsys_file"), 'w') as f:
+                f.write(qsys_file.format(qsys_fill[edition]))
 
-        backend.build()
-        compare_files(ref_dir, work_root, test_sets[edition])
+            backend.configure(args)
+
+            compare_files(ref_dir, work_root, ['Makefile',
+                                               name+'.tcl'])
+
+            backend.build()
+            compare_files(ref_dir, work_root, test_sets[edition][pnr])
---
+++ tests/test_quartus/Pro/quartus_dse.cmd
@@ -0,0 +1,1 @@
+test_quartus_0 --use-dse-file test_quartus_0.dse
