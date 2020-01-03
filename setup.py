8af093d99e79d0a60f7c75de8cf11909ee0e866d
DHWinterWolf <17268847+DHWinterWolf@users.noreply.github.com>
2020-01-03 09:51:59 +0000

    Add the facility to use DSE as a place at route tool for Quartus.
    I've tried to make anything that might be useful within the context of Edalize configurable.
    
    Some things don't make sense to be configurable with an Edalize configuration. For example, since it already runs synthesis and DSE is being set up just for PAR, there is no reason to use existing archives or re-run synthesis.
    
    Other things have been omitted for simplicity. For example, automatic seed generation has been fixed off to avoid making things overly complex. It's very easy to generate a list of seeds when invoking Edalize, either from a FuseSoC core file or any Python code, so having another method to do the same thing with less flexibility seems redundant.

--- setup.py
+++ setup.py
@@ -23,6 +23,7 @@ setup(
         'templates/quartus/quartus-project.tcl.j2',
         'templates/quartus/quartus-std-makefile.j2',
         'templates/quartus/quartus-pro-makefile.j2',
+        'templates/quartus/quartus-dse.j2',
         'templates/trellis/trellis-makefile.j2',
         'templates/ascentlint/Makefile.j2',
         'templates/ascentlint/run-ascentlint.tcl.j2',
