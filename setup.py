bc8364907ead3490bb5076347ab5b5acf9b65a1e
Piotr Binkowski <pbinkowski@antmicro.com>
2020-02-24 11:13:04 +0100

    setup: add vivado-yosys templates
    Signed-off-by: Piotr Binkowski <pbinkowski@antmicro.com>

--- setup.py
+++ setup.py
@@ -21,6 +21,8 @@ setup(
         'templates/vivado/vivado-project.tcl.j2',
         'templates/vivado/vivado-run.tcl.j2',
         'templates/vivado/vivado-synth.tcl.j2',
+        'templates/vivado/vivado-project-yosys.tcl.j2',
+        'templates/vivado/vivado-run-yosys.tcl.j2',
         'templates/vunit/run.py.j2',
         'templates/quartus/quartus-project.tcl.j2',
         'templates/quartus/quartus-std-makefile.j2',
