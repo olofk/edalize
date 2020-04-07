aaaf1d6f5f26cef64765bba16da78c4ded318bb2
GCHQDeveloper560 <48131108+GCHQDeveloper560@users.noreply.github.com>
2020-04-07 17:53:49 +0100

    Add VHDL generic support for xsim

--- tests/test_xsim/config.mk
+++ tests/test_xsim/config.mk
@@ -4,7 +4,7 @@ TOPLEVEL      = top_module
 
 VLOG_DEFINES  = --define vlogdefine_bool=True --define vlogdefine_int=42 --define vlogdefine_str=hello
 VLOG_INCLUDES = -i .
-VLOG_PARAMS   = --generic_top vlogparam_bool=1 --generic_top vlogparam_int=42 --generic_top vlogparam_str=hello
+GEN_PARAMS    = --generic_top vlogparam_bool=1 --generic_top vlogparam_int=42 --generic_top vlogparam_str=hello --generic_top generic_bool=1 --generic_top generic_int=42 --generic_top generic_str=hello
 
 XELAB_OPTIONS =	some xelab_options
 XSIM_OPTIONS  = a few xsim_options
