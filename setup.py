8b07b2e5a37ae678079d2ac4bfede339d722b51d
Alessandro Comodi <acomodi@antmicro.com>
2020-04-10 16:10:57 +0200

    Add nextpnr tool
    Signed-off-by: Alessandro Comodi <acomodi@antmicro.com>

--- setup.py
+++ setup.py
@@ -31,6 +31,7 @@ setup(
         'templates/trellis/trellis-makefile.j2',
         'templates/ascentlint/Makefile.j2',
         'templates/ascentlint/run-ascentlint.tcl.j2',
+        'templates/nextpnr-xilinx/nextpnr-xilinx-makefile.j2',
     ]},
     author = "Olof Kindgren",
     author_email = "olof.kindgren@gmail.com",
