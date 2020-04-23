4bb1a89dde621dc94db7f2917e5d5db09ed7e7c9
Olof Kindgren <olof.kindgren@gmail.com>
2020-04-23 23:11:22 +0200

    Fix broken trellis backend

--- edalize/trellis.py
+++ edalize/trellis.py
@@ -37,8 +37,8 @@ class Trellis(Edatool):
     def configure_main(self):
         # Write yosys script file
         (src_files, incdirs) = self._get_fileset_files()
-        yosys_synth_options = self.tool_options.get('yosys_synth_options', '')
-        yosys_synth_options = "-nomux" + yosys_synth_options
+        yosys_synth_options = self.tool_options.get('yosys_synth_options', [])
+        yosys_synth_options = ["-nomux"] + yosys_synth_options
         yosys_edam = {
                 'files'         : self.files,
                 'name'          : self.name,
@@ -53,7 +53,7 @@ class Trellis(Edatool):
                 }
 
         yosys = getattr(import_module("edalize.yosys"), 'Yosys')(yosys_edam, self.work_root)
-        yosys.configure(self.args)
+        yosys.configure()
 
         lpf_files = []
         for f in src_files:
