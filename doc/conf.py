9b6aa011a72d014afc6626668a52e2baf2c4919b
Philipp Wagner <phw@lowrisc.org>
2020-11-11 18:09:52 +0000

    [doc] Require Sphinx 3 at least
    We use sphinx-autodoc-typehints in our documentation, which needs Sphinx
    3 at least. It is actually listed as dependency there, but pip seems to
    be not overly concerned to get that right.

--- doc/conf.py
+++ doc/conf.py
@@ -36,7 +36,7 @@ release = '0.1.3'
 
 # If your documentation needs a minimal Sphinx version, state it here.
 #
-# needs_sphinx = '1.0'
+needs_sphinx = '3.0'
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
