f8b3f666a282e09b8ce06388101d179f8c70e8d4
Olof Kindgren <olof.kindgren@gmail.com>
2021-03-24 22:26:33 +0100

    Add support for custom launchers
    This add suspport for setting an EDALICE_LAUNCHER environment variable
    to specify a custom command to be run with the rest of the command-line
    passed as arguments.
    
    The main purpose of this is to let the custom launcher add a prefix command
    to the command line. There are several use cases for this such as running
    tools from containers or starting a license server prior to running a tool.
    
    This fix covers a subset of the backends. More backends will be supported
    in time and there will eventually be an option for setting the launcher as
    a command-line option rather than through environment variables.

--- tests/test_vivado/yosys/test_vivado_yosys_0.mk
+++ tests/test_vivado/yosys/test_vivado_yosys_0.mk
@@ -6,11 +6,11 @@ all: $(TARGET).edif
 
 
 %.blif: yosys.tcl
-	yosys -l yosys.log -p "tcl $?"
+	$(EDALIZE_LAUNCHER) yosys -l yosys.log -p "tcl $?"
 %.json: yosys.tcl
-	yosys -l yosys.log -p "tcl $?"
+	$(EDALIZE_LAUNCHER) yosys -l yosys.log -p "tcl $?"
 %.edif: yosys.tcl
-	yosys -l yosys.log -p "tcl $?"
+	$(EDALIZE_LAUNCHER) yosys -l yosys.log -p "tcl $?"
 
 clean:
 	rm -f $(TARGET).blif $(TARGET).json $(TARGET).edif
