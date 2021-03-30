88aa9e00049425d8c69a6f8fb58e2702b5b0565b
Klas Nordmark <klas.nordmark.se@ieee.org>
2021-03-30 09:45:39 +0200

    Made the Openlane backend take a tcl file with parameters for Openlane

--- edalize/openlane.py
+++ edalize/openlane.py
@@ -16,40 +16,24 @@ class Openlane(Edatool):
     def get_doc(cls, api_ver):
         if api_ver == 0:
             return {'description' : "Open source flow for ASIC synthesis, placement and routing",
-                    'members': [
-                        {'name' : 'clk_name',
-                        'type' : 'String',
-                        'desc' : 'Name of the clock signal in the design.'},
-                        {'name' : 'clk_period',
-                        'type' : 'String',
-                        'desc' : 'Period in nanoseconds of the design clock'},
-                    ],
+                    'members': [],
                     'lists' : []}
 
-    # At a minimum, we need our source Verilog files and locations to run Openlane
-    # For a minimal example, we can leave everything else as default (to be changed later)
     def configure_main(self):
         file_table = ""
+        tcl_params = ""
         (src_files, incdirs) = self._get_fileset_files()
         for f in src_files:
-            file_table = file_table + f.name + " "
-
-        clk_name = self.tool_options.get('clk_name')
-        if clk_name == None:
-            logger.error("Openlane backend must have the tool option clk_name set.")
-            return
-
-        clk_period = self.tool_options.get('clk_period')
-        if clk_period == None:
-            logger.error("Openlane backend must have the tool option clk_period set (in nanoseconds).")
-            return
+            if f.file_type.startswith('verilogSource'):
+                file_table = file_table + f.name + " "
+            elif f.file_type.startswith('tclSource'):
+                tcl_params = f.name
 
         template_vars = {
             'top' : self.toplevel,
             'file_table' : file_table,
             'work_root' : os.path.split(self.work_root)[1],
-            'clk_name' : clk_name,
-            'clk_period' : clk_period,
+            'tcl_params' : tcl_params,
         }
 
         script_name = 'config.tcl'
