4646760a16784c404009e5e55a97cbfa00755145
Tianrui Wei <tianrui-wei@outlook.com>
2021-08-21 19:04:02 +0800

    [feat] add user specified option support
    Signed-off-by: Tianrui Wei <tianrui-wei@outlook.com>

--- edalize/slang.py
+++ edalize/slang.py
@@ -24,13 +24,16 @@ Example snippet of a CAPI2 description file for Slang:
     slang:
       mode:
         - lint # this will lint all the files
-        - preprocess # this will preprocess all the files and output a single file  
+        - preprocess # this will preprocess all the files and output a single file
+      extra_options:
+        user defined options
 
     """
 
     tool_options = {
         'lists' : {
-            'mode': 'String'
+            'mode': 'String',
+            'extra_options': 'String'
         }
     }
 
@@ -63,6 +66,11 @@ Example snippet of a CAPI2 description file for Slang:
                         'name': 'mode',
                         'type': 'String',
                         'desc': ("choose slang to run in either lint mode or preprocess mode")
+                    },
+                    {
+                        'name': 'extra_options',
+                        'type': 'String',
+                        'desc': ("extra options for slang")
                     }
                 ]
             }
@@ -83,11 +91,10 @@ Example snippet of a CAPI2 description file for Slang:
         '''
         get the current running mode: whether run to preprocess or lint
         '''
-        run_mode = self.tool_options.get('mode', "lint")
+        run_mode = self.tool_options.get('mode', '')
         if run_mode == "lint":
             self.flags += ["--lint-only"]
-        else :
-            assert(run_mode == "preprocess")
+        elif run_mode == "preprocess" :
             self.flags += ["--preprocess"]
 
 
@@ -97,6 +104,13 @@ Example snippet of a CAPI2 description file for Slang:
         '''
         for key, value in self.vlogdefine.items():
             self.flags.append(' -D {}={} '.format(key, self._param_value_str(value)))
+
+    def _get_extra_options(self):
+        '''
+        get extra options from user
+        '''
+        extra_options = self.tool_options.get('extra_options', "")
+        self.flags += extra_options.split()
 
     def _get_top_flags(self):
         '''
@@ -116,5 +130,6 @@ Example snippet of a CAPI2 description file for Slang:
         self._get_define_flags()
         self._get_run_mode_flags()
         self._get_file_names()
+        self._get_extra_options()
         self._run_tool("slang" ,self.flags)
         
