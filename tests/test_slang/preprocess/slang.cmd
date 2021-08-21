9df6c13e03e6ae4463c6653531b922b0fc0ca319
Tianrui Wei <tianrui-wei@outlook.com>
2021-08-21 14:51:12 +0000

    [fix] fix python3 subprocess error
    Signed-off-by: Tianrui Wei <tianrui-wei@outlook.com>

--- tests/test_slang/preprocess/slang.cmd
+++ tests/test_slang/preprocess/slang.cmd
@@ -1,1 +1,1 @@
---top top_module -D vlogdefine_bool=1 -D vlogdefine_int=42 -D vlogdefine_str=hello --preprocess -I . sv_file.sv vlog_file.v vlog05_file.v another_sv_file.sv
+-D vlogdefine_bool=1 -D vlogdefine_int=42 -D vlogdefine_str=hello -I . sv_file.sv vlog_file.v vlog05_file.v another_sv_file.sv --preprocess --top top_module
