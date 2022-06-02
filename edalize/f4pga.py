98f8fac9ffdcaf29b6a3f135c0ab076d1ec04895
Adam Taylor <afranklint16@gmail.com>
2022-06-02 11:00:40 -0600

    Added f4pga base module and edited flow module

---
+++ edalize/f4pga.py
@@ -0,0 +1,28 @@
+# Copyright edalize contributors
+# Licensed under the 2-Clause BSD License, see LICENSE for details.
+# SPDX-License-Identifier: BSD-2-Clause
+
+from edalize.edatool import Edatool
+from edalize.flows.f4pga import F4pga as F4PGA_Flow
+
+class F4pga(Edatool):
+    """
+    F4PGA backend (Edatool portion)
+
+    Calls the tools that F4PGA uses with the specified options
+    """
+
+    @classmethod
+    def get_doc(cls, api_ver):
+        if api_ver == 0:
+            return {
+                "description" : "F4PGA Edatool backend (Yosys and VPR)"
+            }
+
+    def __init__(self, edam=None, work_root=None, eda_api=None, verbose=True):
+        super().__init__(edam, work_root, eda_api, verbose)
+        edam["flow_options"] = edam["tool_options"]["f4pga"]
+        self.f4pga = F4PGA_Flow(edam, work_root, verbose)
+
+    def configure_main(self):
+        self.f4pga.configure()
--- edalize/flows/f4pga.py
+++ edalize/flows/f4pga.py
@@ -6,17 +6,27 @@ import os.path
 
 from edalize.flows.edaflow import Edaflow
 
-class F4PGA(Edaflow):
-    """Free and open-source 'flow for FPGA's'. Uses Yosys for synthesys and VPR or NextPNR for place and route."""
+class F4pga(Edaflow):
+    """
+    Free and open-source 'flow for FPGA's'. 
+    
+    Uses Yosys for synthesys and VPR or NextPNR for place and route.
+    """
 
     FLOW = [
-        ("yosys", ["vpr"], {"arch":"", "output_format":"json"}),
-        ("vpr", [""], {"arch":""})
+        ("yosys", ["vpr"], {"arch": "xilinx", "output_format": "blif"}),
+        ("vpr", [], {"arch": "xilinx", "arch_xml": "${shareDir}/arch/xc7a50t_test/arch.timing.xml", "vpr_options": []})
     ]
 
     FLOW_OPTIONS = {}
 
+    def __init__(self, edam, work_root, verbose=False):
+        Edaflow.__init__(self, edam, work_root, verbose)
+
+    def build_tool_graph(self):
+        return super().build_tool_graph()
+
     def configure_tools(self, nodes):
         super().configure_tools(nodes)
-
-        # Add FASM and bitstream generation to makefile
+        name = self.edam["name"]
+        self.commands.set_default_target(name + ".bit")
