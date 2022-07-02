28953fbdc0a20b702c24cd44a4777bba3e333208
Olof Kindgren <olof.kindgren@gmail.com>
2022-07-02 23:13:36 +0200

    Refactor and introduce get_tool_options in flow
    Before this change, there was no meaningful way to use flow options
    because it was not possible to query or set tool options for tools
    outside of the ones in the static FLOW list.
    
    This patch separates the process of applying options into two steps.
    A user can first use get_flow_options to get flow options and then
    use get_tool_options with the preferred flow options set, to get
    all tool options for a configured flow.
    
    The redefinition of get_flow_options to only return flow options
    (and not also tool options like previously) is an API break but will
    hopefully not cause too much trouble.

--- edalize/flows/lint.py
+++ edalize/flows/lint.py
@@ -3,6 +3,7 @@
 # SPDX-License-Identifier: BSD-2-Clause
 
 import os.path
+from importlib import import_module
 
 from edalize.flows.edaflow import Edaflow
 
@@ -12,10 +13,10 @@ class Lint(Edaflow):
 
     argtypes = ["vlogdefine", "vlogparam"]
 
-    FLOW = [
-        ("verilator", [], {"mode": "lint-only", "exe": "false", "make_options": []}),
+    FLOW_DEFINED_TOOL_OPTIONS = {
+        "verilator": {"mode": "lint-only", "exe": "false", "make_options": []},
         # verible, spyglass, ascentlint, slang...
-    ]
+    }
 
     FLOW_OPTIONS = {
         "frontends": {
@@ -29,23 +30,28 @@ class Lint(Edaflow):
         },
     }
 
-    def build_tool_graph(self):
+    @classmethod
+    def get_tool_options(cls, flow_options):
+        flow = flow_options.get("frontends", [])
+        tool = flow_options.get("tool")
+        if not tool:
+            raise RuntimeError("Flow 'lint' requires flow option 'tool' to be set")
+        flow.append(tool)
+
+        return cls.get_filtered_tool_options(flow, cls.FLOW_DEFINED_TOOL_OPTIONS)
+
+    def configure_flow(self, flow_options):
         tool = self.flow_options.get("tool", "")
         if not tool:
-            raise RuntimeError(
-                "'lint' backend requires flow option 'tool' to be defined"
-            )
-        tmp = [x for x in self.FLOW if x[0] == tool]
-        self.FLOW = tmp
+            raise RuntimeError("Flow 'lint' requires flow option 'tool' to be set")
+        flow = [(tool, [], self.FLOW_DEFINED_TOOL_OPTIONS.get(tool, {}))]
+        # Add any user-specified frontends to the flow
+        next_tool = tool
 
-        # Note: This makes an assumption that the first tool in self.FLOW is
-        # a single entry point to the flow
-        next_tool = self.FLOW[0][0]
-
-        for frontend in reversed(self.flow_options.get("frontends", [])):
-            self.FLOW[0:0] = [(frontend, [next_tool], {})]
+        for frontend in reversed(flow_options.get("frontends", [])):
+            flow[0:0] = [(frontend, [next_tool], {})]
             next_tool = frontend
-        return super().build_tool_graph()
+        return flow
 
     def configure_tools(self, nodes):
         super().configure_tools(nodes)
