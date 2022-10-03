743319ff5aae2f4b92a0816cd3291b8833499080
Adam Taylor <afranklint16@gmail.com>
2022-10-03 09:12:17 -0600

    Create F4PGA flow

---
+++ edalize/flows/f4pga.py
@@ -0,0 +1,11 @@
+# Copyright edalize contributors
+# Licensed under the 2-Clause BSD License, see LICENSE for details.
+# SPDX-License-Identifier: BSD-2-Clause
+
+import os.path
+
+from edalize.flows.edaflow import Edaflow
+
+
+class F4pga(Edaflow):
+    """Edalize implementation of F4PGA (Free and Open-Source Flow For FPGAs)"""
