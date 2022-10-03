16c6156531f61abab0b73ca293fcb393f2bfbaa8
Adam Taylor <afranklint16@gmail.com>
2022-10-03 09:24:34 -0600

    Add first option and flow configuration

--- edalize/flows/f4pga.py
+++ edalize/flows/f4pga.py
@@ -9,3 +9,26 @@ from edalize.flows.edaflow import Edaflow
 
 class F4pga(Edaflow):
     """Edalize implementation of F4PGA (Free and Open-Source Flow For FPGAs)"""
+
+    """Currently supports Xilinx 7-series chips"""
+
+    # F4PGA defined inputs to the underlying tools
+    FLOW_DEFINED_TOOL_OPTIONS = {}
+
+    # Inputs to the F4PGA flow
+    FLOW_OPTIONS = {
+        # Optional, defaults to VPR
+        "pnr": {
+            "type": "str",
+            "desc": "Place and route tool. Valid options are 'vpr'/'vtr' and 'nextpnr'. Defaults to VPR.",
+        }
+    }
+
+    # Define which tools are called and in what order
+    def configure_flow(self, flow_options):
+        flow = ["yosys"]
+        if flow_options.get("pnr") in ["nextpnr"]:
+            flow += ["nextpnr"]
+        else:
+            flow += ["vpr"]
+        return flow
