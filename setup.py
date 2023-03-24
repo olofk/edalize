8473fe13a1abffe013fae8d7716bba856d770efe
Olof Kindgren <olof.kindgren@gmail.com>
2023-03-24 22:58:42 +0100

    Prepare for release

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def read(fname):
 
 setup(
     name="edalize",
-    version="0.4.1",
+    version="0.5.0",
     packages=["edalize", "edalize.tools", "edalize.flows"],
     package_data={
         "edalize": [
