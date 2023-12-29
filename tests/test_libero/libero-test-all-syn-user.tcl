df15cb7e46fca8effca5f00a0397c777fe31466c
Liam Beguin <liambeguin@gmail.com>
2023-12-29 00:17:28 -0500

    libero: properly escape string parameters
    This was causing string parameters to be ignored during synthesis,
    preventing things like:
    
    	fusesoc run servant --target=polarfire_splashkit --memfile=anything.hex
    
    Signed-off-by: Liam Beguin <liambeguin@gmail.com>

--- tests/test_libero/libero-test-all-syn-user.tcl
+++ tests/test_libero/libero-test-all-syn-user.tcl
@@ -4,9 +4,9 @@
 
 set_option -hdl_param -set vlogparam_bool=true
 set_option -hdl_param -set vlogparam_int=42
-set_option -hdl_param -set vlogparam_str=hello
+set_option -hdl_param -set vlogparam_str="hello"
 set_option -hdl_define -set vlogdefine_bool=true
 set_option -hdl_define -set vlogdefine_int=42
-set_option -hdl_define -set vlogdefine_str=hello
+set_option -hdl_define -set vlogdefine_str="hello"
 
 set_option -include_path {../../.}
