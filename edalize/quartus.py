dc95b0b3b92f4065a75152736b6e5142a8e0cc63
GCHQDeveloper137 <74659744+GCHQDeveloper137@users.noreply.github.com>
2024-03-08 20:17:39 +0000

    Handle the new XML format for QSys from version 23.
    * Parse QSys generated with v23 and newer
    
    * Update tests and ensure new format standard files are matched correctly
    
    ---------
    
    Co-authored-by: GCHQDeveloper137 <qchqdeveloper137@outlook.com>

--- edalize/quartus.py
+++ edalize/quartus.py
@@ -184,13 +184,20 @@ class Quartus(Edatool):
             try:
                 qsysTree = ET.parse(os.path.join(self.work_root, f.name))
                 try:
-                    tool = qsysTree.find("component").attrib["tool"]
-                    if tool == "QsysPro" and self.isPro:
+                    try:
+                        tool = qsysTree.find("component").attrib["tool"]
+                    except AttributeError:
+                        tool = qsysTree.find(".//{http://www.altera.com/XMLSchema/IPXact2014/extensions}tool").text
+                    if tool == "QsysPro":
+                        if self.isPro:
+                            name = f.name
+                    elif not self.isPro:
                         name = f.name
                 except (AttributeError, KeyError):
                     # Either a component wasn't found in the QSYS file, or it
                     # had no associated tool information. Make the assumption
-                    # it was a Standard edition file
+                    # it is a Standard edition file, as the old formats just
+                    # don't specify.
                     if not self.isPro:
                         name = f.name
             except (ET.ParseError, IOError):
