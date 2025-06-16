4b1a72cffe0a25a8e4a9002c11df19e0bc37ab21
Olof Kindgren <olof.kindgren@gmail.com>
2025-06-16 12:56:48 +0200

    vcs: Rewrite 3-stage mode
    The VCS 3-stage mode tried to be clever by doing parallel vlogan
    calls from a precalculated dependency tree. This, however, turned
    out to be a bit too clever for its own good and failed on several
    corner cases. The rewritten backend executes vlogan/vhdlan commands
    serially instead and should be safer to use.

--- tests/tools/vcs/basic/work_2.f
+++ tests/tools/vcs/basic/work_2.f
@@ -1,1 +1,1 @@
-several analysis options also vhdlan options
+several analysis options a few vlogan options +define+vlogdefine_bool=1 +define+vlogdefine_int=42 +define+vlogdefine_str=""hello"" +define+FD_KEY=""FD_VAL"" +incdir+.
--- tests/tools/vcs/minimal/work_2.f
+++ tests/tools/vcs/minimal/work_2.f
@@ -1,1 +1,1 @@
-
++define+FD_KEY=""FD_VAL"" +incdir+.
