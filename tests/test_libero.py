fa5634d6ae92dce919cac35223569c6919998ee8
Liam Beguin <liambeguin@gmail.com>
2025-12-02 17:06:17 -0500

    libero: tests: pass in custom set of files
    Pass in a custom set of files to avoid getting an unsupported extension
    error when Libero tries to generate a project.
    
    Error log:
    
    	Error:  '' is not a supported extension.
    	Error:  The command 'create_links' failed.
    	Error:  Failure when executing Tcl script. [ Line 22 ]
    	Error:  The Execute Script command failed.
    	The libero-test project was closed.
    
    Signed-off-by: Liam Beguin <liambeguin@gmail.com>

--- tests/test_libero.py
+++ tests/test_libero.py
@@ -1,12 +1,29 @@
 from .edalize_common import make_edalize_test
 
+libero_files=[
+    {"name": "sdc_file.sdc", "file_type": "SDC"},
+    {"name": "sv_file.sv", "file_type": "systemVerilogSource"},
+    {"name": "vlog_file.v", "file_type": "verilogSource"},
+    {"name": "tcl_file.tcl", "file_type": "tclSource"},
+    {
+        "name": "vlog_with_define.v",
+        "file_type": "verilogSource",
+        "define": {"FD_KEY": "FD_VAL"},
+    },
+    {"name": "vlog05_file.v", "file_type": "verilogSource-2005"},
+    {"name": "vhdl_file.vhd", "file_type": "vhdlSource"},
+    {"name": "vhdl_lfile", "file_type": "vhdlSource", "logical_name": "libx"},
+    {"name": "another_sv_file.sv", "file_type": "systemVerilogSource"},
+    {"name": "pdc_constraint_file.pdc", "file_type": "PDC"},
+    {"name": "pdc_floorplan_constraint_file.pdc", "file_type": "FPPDC"},
+]
 
 def test_libero(make_edalize_test):
     """Test passing tool options to the Libero backend"""
     name = "libero-test"
     tool_options = {"family": "PolarFire", "die": "MPF300TS_ES", "package": "FCG1152"}
 
-    tf = make_edalize_test("libero", test_name=name, tool_options=tool_options)
+    tf = make_edalize_test("libero", test_name=name, tool_options=tool_options, files=libero_files)
 
     tf.backend.configure()
     tf.compare_files(
@@ -35,7 +52,7 @@ def test_libero_with_params(make_edalize_test):
         "flashpro5_freq": "15000000",
     }
 
-    tf = make_edalize_test("libero", test_name=name, tool_options=tool_options)
+    tf = make_edalize_test("libero", test_name=name, tool_options=tool_options, files=libero_files)
 
     tf.backend.configure()
     tf.compare_files(
